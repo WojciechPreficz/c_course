e61cf4a0e84d7f676f71da3950bffb04182d12ee
wojciech preficz <wojciech.preficz@gmail.com>
2019-11-15 11:09:42 +0100

    Add files via upload

---
+++ Linq/EmployeeLinqTests/EmployeeFunctionsTests.cs
@@ -0,0 +1,135 @@
+using EmployeeManagement;
+using EmployeePersistence;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace EmployeeLinqTests
+{
+    public class EmployeeFunctionsTests
+    {
+        private readonly IEnumerable<Employee> employees;
+
+        public EmployeeFunctionsTests()
+        {
+            var employeeRepository = new EmployeeRepository();
+            employees = employeeRepository.Employees();
+        }
+
+        [Fact]
+        public void CountEmployeesOlderThan50()
+        {
+            var result = EmployeeFunctions.CountEmployeesOlderThan50(employees);
+
+            result.Should().Be(483);
+        }
+
+        [Fact]
+        public void ListSurnamesOfEmployeesEarningOver9900()
+        {
+            var result = EmployeeFunctions.ListSurnamesOfEmployeesEarningOver9900(employees);
+
+            var expectedResult = new List<string>
+            {
+                "Licari", "Andrew", "Noyes", "MacLucais", "Worham",
+                "Grimsdith", "Blundan", "Gorstidge", "McAvinchey", "O'Deoran"
+            };
+
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Fact]
+        public void FindEmployeesYoungerThan18AndEarningLessThan1100()
+        {
+            var result = EmployeeFunctions.FindEmployeesYoungerThan18AndEarningLessThan1100(employees);
+
+            var expectedResult = new List<Employee>
+            {
+                new Employee
+                {
+                    Age = 10,
+                    Id = 188,
+                    Name = "Bondy",
+                    Surname = "McGloughlin",
+                    Wage = 1054
+                }
+            };
+
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Fact]
+        public void Find_NameAndSurname_OfTheOldestEmployeeWhoEarnsBetween6500And7000()
+        {
+            var result = EmployeeFunctions.Find_NameAndSurname_OfTheOldestEmployeeWhoEarnsBetween6500And7000(employees);
+
+            result.Should().BeEquivalentTo("Marya Oolahan");
+        }
+
+        [Fact]
+        public void EmployeesUnderAgeOf18Exist_ReturnsTrue_IfThereAreEmployeesUnderAgeOf18()
+        {
+            var result = EmployeeFunctions.EmployeesUnderAgeOf18Exist(employees);
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void EmployeeWithoutSurnameExists_ReturnsFalse_IfAllEmployeesHaveSurname()
+        {
+            var result = EmployeeFunctions.EmployeeWithoutSurnameExists(employees);
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void AverageWageWithoutTop100andBottom100Wages()
+        {
+            var result = EmployeeFunctions.AverageWageWithoutTop100andBottom100Wages(employees);
+
+            result.Should().Be(5405.55);
+        }
+
+        [Fact]
+        public void FindTheMostPopularNameStartingWithTheOldestLetterOfTheAlphabet()
+        {
+            var result = EmployeeFunctions.FindTheMostPopularNameStartingWithTheOldestLetterOfTheAlphabet(employees);
+            result.Should().Be("Virge");
+        }
+
+        [Fact]
+        public void FindTheNumberOfEmployeesWhosSalaryIsDivisibleByTheirId()
+        {
+            var result = EmployeeFunctions.FindTheNumberOfEmployeesWhosSalaryIsDivisibleByTheirId(employees);
+            result.Should().Be(7);
+        }
+
+        [Fact]
+        public void FindTheAgeDifferenceBetweenTheEldestAndYoungestEmployee()
+        {
+            var result = EmployeeFunctions.FindTheAgeDifferenceBetweenTheEldestAndYoungestEmployee(employees);
+            result.Should().Be(99);
+        }
+
+        [Fact]
+        public void FindTheNumberOfEmployeesWhosNameIsLongerThenSurname()
+        {
+            var result = EmployeeFunctions.FindTheNumberOfEmployeesWhosNameIsLongerThenSurname(employees);
+            result.Should().Be(276);
+        }
+
+        [Fact]
+        public void FindTheYoungestEmployeeNameWithTheHighestWage()
+        {
+            var result = EmployeeFunctions.FindTheYoungestEmployeeNameWithTheHighestWage(employees);
+            result.Should().Be("Brittaney");
+        }
+
+        [Fact]
+        public void FindTheThirdBestWageEmployeeNameWithSurnameLonger5()
+        {
+            var result = EmployeeFunctions.FindTheThirdBestWageEmployeeNameWithSurnameLonger5(employees);
+            result.Should().Be("Susie");
+        }
+    }
+}
---
+++ Linq/EmployeeManagement/EmployeeFunctions.cs
@@ -0,0 +1,81 @@
+﻿using EmployeePersistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EmployeeManagement
+{
+    public static class EmployeeFunctions
+    {
+        public static int CountEmployeesOlderThan50(IEnumerable<Employee> employees)
+        {
+            return employees.Where(employee => employee.Age > 50).Count();   
+        }
+
+        public static IEnumerable<string> ListSurnamesOfEmployeesEarningOver9900(IEnumerable<Employee> employees)
+        {
+            return employees.Where(employee => employee.Wage > 9900).Select(emp => emp.Surname);
+        }
+
+        public static List<Employee> FindEmployeesYoungerThan18AndEarningLessThan1100(IEnumerable<Employee> employees)
+        {
+            return employees.Where(emp => emp.Age < 18).Where(emp => emp.Wage < 1100).ToList();
+        }
+
+        public static string Find_NameAndSurname_OfTheOldestEmployeeWhoEarnsBetween6500And7000(IEnumerable<Employee> employees)
+        {
+            return employees.Where(emp => (emp.Wage > 6500 && emp.Wage < 7000)).OrderByDescending(emp => emp.Age).Select(emp => emp.Name + " " + emp.Surname).First().ToString();
+        }
+
+        public static bool EmployeesUnderAgeOf18Exist(IEnumerable<Employee> employees)
+        {
+            return employees.Where(emp => emp.Age < 18).Any();
+        }
+
+        public static bool EmployeeWithoutSurnameExists(IEnumerable<Employee> employees)
+        {
+            return employees.Where(emp => emp.Surname == "").Any();
+        }
+
+        public static double AverageWageWithoutTop100andBottom100Wages(IEnumerable<Employee> employees)
+        {
+            double sum = employees.OrderBy(emp => emp.Wage).Skip(100).OrderByDescending(emp => emp.Wage).Skip(100).Sum(emp => emp.Wage);
+            double count = employees.Count() - 200;
+
+            return sum / count;
+        }
+
+        public static string FindTheMostPopularNameStartingWithTheOldestLetterOfTheAlphabet(IEnumerable<Employee> employees)
+        {
+            return employees.GroupBy(emp => emp.Name).OrderByDescending(emp => emp.Count()).ThenByDescending(emp => emp.Key).Take(1).Select(emp => emp.Key).First();
+        }
+
+        public static int FindTheNumberOfEmployeesWhosSalaryIsDivisibleByTheirId(IEnumerable<Employee> employees)
+        {            
+            return employees.Where(emp => emp.Wage % emp.Id == 0).Count();
+        }
+
+        public static int FindTheAgeDifferenceBetweenTheEldestAndYoungestEmployee(IEnumerable<Employee> employees)
+        {
+            var youngest = employees.OrderBy(emp => emp.Age).Select(emp => emp.Age).First();
+            var oldest = employees.OrderByDescending(emp => emp.Age).Select(emp => emp.Age).First();
+
+            return oldest - youngest;
+        }
+
+        public static int FindTheNumberOfEmployeesWhosNameIsLongerThenSurname(IEnumerable<Employee> employees)
+        {
+            return employees.Where(emp => emp.Name.Length > emp.Surname.Length).Count();
+        }
+
+        public static string FindTheYoungestEmployeeNameWithTheHighestWage(IEnumerable<Employee> employees)
+        {            
+            return employees.OrderBy(emp => emp.Age).ThenByDescending(emp => emp.Wage).Select(emp => emp.Name).First();
+        }
+
+        public static string FindTheThirdBestWageEmployeeNameWithSurnameLonger5(IEnumerable<Employee> employees)
+        {
+            return employees.Where(emp => emp.Surname.Length > 5).OrderByDescending(emp => emp.Wage).Skip(2).Select(emp => emp.Name).First();
+        }
+    }
+}
